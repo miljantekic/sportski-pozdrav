66e33d76ceee4cb7fbb7a2d32ec8a98d70261743
Miljan Tekic <miljan.tekic@gmail.com>
2020-08-10 17:35:05 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import _ from 'lodash';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sportEmojis = [
+    '⛷️',
+    '🏂',
+    '🏇',
+    '🏄‍♂️',
+    '🏊‍♂️',
+    '🥌',
+    '⛸️',
+    '🏓',
+    '🥊',
+    '🎾',
+    '🏈',
+    '🏐',
+    '🏀',
+    '⚾',
+    '⚽',
+];
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        const emojis = _.shuffle(sportEmojis);
+
+        this.state = {
+            emojis,
+            inProgress: true,
+            currentEmoji: 0,
+        };
+    }
+
+    componentDidMount() {
+        const {emojis} = this.state;
+
+        let emoji = 0;
+
+        const interval = setInterval(() => {
+            emoji++;
+
+            if (emoji >= emojis.length) {
+                emoji = 0;
+            }
+
+            this.setState({
+                currentEmoji: emoji,
+            });
+        }, 80);
+
+        console.log(interval);
+
+        setTimeout(() => {
+            clearInterval(interval);
+        }, _.random(1000, 1500))
+    }
+
+    render() {
+        const {emojis, currentEmoji} = this.state;
+
+        return <div className="PozdravApp">
+            <div className="SlotSpinner SpinnerSide">
+                <span  role="img" aria-label="sportski">{emojis[currentEmoji]}</span>
+            </div>
+            <div className="WaveSpinner SpinnerSide">
+                <span role="img" aria-label="pozdrav">👋</span>
+            </div>
+        </div>
+    }
 }
 
 export default App;
